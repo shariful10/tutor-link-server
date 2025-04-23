2b91136d4d5dbb5ba035efacdff9442e942f5897
shariful10 <freelancershariful10@gmail.com>
2025-04-24 01:48:54 +0600

    Refector server.ts file

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function server() {
     await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port} 🏃🏽‍♂️‍➡️`)
+      console.log(`🚀 App is running on port ${config.port}`)
     })
   } catch (error) {
     console.error(error)

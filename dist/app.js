89d7a72491439735db5cba9ebf3362918d44472c
shariful10 <freelancershariful10@gmail.com>
2025-04-25 23:08:37 +0600

    Modify

--- dist/app.js
+++ dist/app.js
@@ -3,25 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_1 = __importDefault(require("http-status"));
-const express_1 = __importDefault(require("express"));
-const auth_route_1 = __importDefault(require("./module/auth/auth.route"));
-const user_router_1 = __importDefault(require("./module/user/user.router"));
 const cors_1 = __importDefault(require("cors"));
-const request_router_1 = __importDefault(require("./module/sendRequestTuror/request.router"));
+const express_1 = __importDefault(require("express"));
+const http_status_1 = __importDefault(require("http-status"));
+const auth_route_1 = __importDefault(require("./module/auth/auth.route"));
 const paymentRouter_1 = __importDefault(require("./module/payments/paymentRouter"));
 const review_router_1 = __importDefault(require("./module/review/review.router"));
+const request_router_1 = __importDefault(require("./module/sendRequestTuror/request.router"));
+const user_router_1 = __importDefault(require("./module/user/user.router"));
 const app = (0, express_1.default)();
 // middleware
 app.use(express_1.default.json());
-// app.use(cors({ origin: "https://tutors-x-new.vercel.app", credentials: true }));
-app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
-// router 
+app.use((0, cors_1.default)({ origin: 'https://tutor-link-client0.vercel.app', credentials: true }));
+// router
 app.use('/api/user', user_router_1.default);
 app.use('/api/auth', auth_route_1.default);
-app.use("/api/requests", request_router_1.default);
-app.use("/api", paymentRouter_1.default);
-app.use("/api", review_router_1.default);
+app.use('/api/requests', request_router_1.default);
+app.use('/api', paymentRouter_1.default);
+app.use('/api', review_router_1.default);
 app.get('/', (req, res) => {
     res.send({
         status: true,
@@ -35,10 +34,10 @@ app.use((err, req, res, _next) => {
         .status(http_status_1.default.INTERNAL_SERVER_ERROR)
         .json({ success: false, message: err.message, error: err });
 });
-app.use("*", (req, res) => {
+app.use('*', (req, res) => {
     res.status(404).json({
         status: false,
-        message: 'Route not found'
+        message: 'Route not found',
     });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,24 @@
-import httpStatus from 'http-status';
+import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
-import authRoute from './module/auth/auth.route';
-import userRouter from './module/user/user.router';
-import cors from "cors";
-import requestRouter from './module/sendRequestTuror/request.router';
-import paymentRouter from './module/payments/paymentRouter';
-import reviewRouter from './module/review/review.router';
+import httpStatus from 'http-status'
+import authRoute from './module/auth/auth.route'
+import paymentRouter from './module/payments/paymentRouter'
+import reviewRouter from './module/review/review.router'
+import requestRouter from './module/sendRequestTuror/request.router'
+import userRouter from './module/user/user.router'
 
 const app = express()
 // middleware
 app.use(express.json())
-// app.use(cors({ origin: "https://tutors-x-new.vercel.app", credentials: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-// router 
+app.use(
+  cors({ origin: 'https://tutor-link-client0.vercel.app', credentials: true })
+)
+// router
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRoute)
-app.use("/api/requests", requestRouter);
-app.use("/api", paymentRouter);
-app.use("/api", reviewRouter);
-
-
+app.use('/api/requests', requestRouter)
+app.use('/api', paymentRouter)
+app.use('/api', reviewRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
@@ -32,14 +31,14 @@ app.get('/', (req: Request, res: Response) => {
 app.use((err: any, req: Request, res: Response, _next: NextFunction) => {
   // console.log('error from app.ts', err)
   res
-    .status(httpStatus.INTERNAL_SERVER_ERROR,)
+    .status(httpStatus.INTERNAL_SERVER_ERROR)
     .json({ success: false, message: err.message, error: err })
 })
 
-app.use("*", (req: Request, res: Response) => {
+app.use('*', (req: Request, res: Response) => {
   res.status(404).json({
     status: false,
-    message: 'Route not found'
+    message: 'Route not found',
   })
 })
 
